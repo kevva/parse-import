f1819946562b4f6aed1091a29ec1a7cd3610b7e5
Kevin Martensson <kevinmartensson@gmail.com>
2015-01-28 18:44:32 +0100

    Tweaks

--- index.js
+++ index.js
@@ -2,13 +2,6 @@
 
 var getImports = require('get-imports');
 
-/**
- * Get path from @import
- *
- * @param {String} str
- * @api private
- */
-
 function path(str) {
 	return /(?:url\()(?:.*?)(?:\))|(["\'])(?:[^"\')]+)\1/ig.exec(str)[0]
 		.replace(/(?:url\()/ig, '')
@@ -17,25 +10,11 @@ function path(str) {
 		.trim();
 }
 
-/**
- * Get condition from @import
- *
- * @param {String} str
- * @api private
- */
-
 function condition(str) {
 	return str.replace(/(?:url\()(?:.*?)(?:\))|(["\'])(?:[^"\')]+)\1/ig, '')
 		.replace(/(?:@import)(?:\s)*/g, '')
 		.trim();
 }
-
-/**
- * Parse @import statements
- *
- * @param {String} str
- * @api public
- */
 
 module.exports = function (str) {
 	var imports = getImports(str);
